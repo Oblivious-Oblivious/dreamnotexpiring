90f506627cb1d58f4da3e9570ddfa3123ff5ae86
Oblivious <thanos.papap42@gmail.com>
2023-02-19 01:36:56 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,4 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
     integrations: [svelte()],
-    site: "https://Oblivious-Oblivious.github.io",
-    base: "/dreamnotexpiring"
 });
