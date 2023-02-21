f0f58b4f34e94f520e26843545dd8e236a778e52
Oblivious <thanos.papap42@gmail.com>
2023-02-21 21:14:02 +0200

    Abstracted PageLifecycle into its own component.
    No need to client load the whole website, rather just the onMount code.

---
+++ src/components/PageLifecycle.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let current_page;
+
+    import { onMount } from "svelte";
+    import { underline_value } from "./Header/underline_value";
+
+    onMount(() => {
+        $underline_value = current_page;
+    });
+
+    let class_prop_warning_fix = "";
+    export { class_prop_warning_fix as class };
+</script>
+
+<div class:class_prop_warning_fix></div>
