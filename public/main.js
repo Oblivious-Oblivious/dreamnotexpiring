7d1089416d2ac350b495da02db52ee1d9dd1f2a0
Oblivious <thanos.papap42@gmail.com>
2023-02-28 04:02:31 +0200

    Implemented correct version of swup with swup head and js plugins.
    Can now use GSAP timelines for swup animations.

--- public/main.js
+++ public/main.js
@@ -1,1 +1,28 @@
-turbo.start();
+const options = [{
+    from: "(.*)",
+    to: "(.*)",
+    in: (next, infos) => {
+        document.querySelector("#swup").style.opacity = 0;
+        gsap.to(document.querySelector("#swup"), {
+            duration: 1,
+            opacity: 1,
+            onComplete: next,
+        });
+    },
+    out: (next, infos) => {
+        document.querySelector("#swup").style.opacity = 1;
+        gsap.to(document.querySelector("#swup"), 0.5, {
+            duration: 1,
+            opacity: 0,
+            onComplete: next,
+        });
+    },
+}];
+
+const swup = new Swup({
+    animateHistoryBrowsing: true,
+    plugins: [
+        new SwupHeadPlugin(),
+        new SwupJsPlugin(options),
+    ],
+});
