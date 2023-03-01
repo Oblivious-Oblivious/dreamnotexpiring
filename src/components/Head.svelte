0b986db246c9777a5f409037b2c336e93b030277
Oblivious <thanos.papap42@gmail.com>
2023-03-01 23:14:29 +0200

    Moved imports from CDNs to npm.

--- src/components/Head.svelte
+++ src/components/Head.svelte
@@ -8,13 +8,6 @@
     <title>{title}</title>
     <meta name="description" content={"Thanasis's Portfolio"}>
     <link rel="icon" type="image/svg+xml" href="/favicon.svg">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.4/gsap.min.js"></script>
-    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.4/ScrollTrigger.min.js"></script> -->
-    <script src="https://unpkg.com/swup@3"></script>
-    <script src="https://unpkg.com/@swup/head-plugin/dist/SwupHeadPlugin.min.js"></script>
-    <script src="https://unpkg.com/@swup/head-plugin/dist/SwupHeadPlugin.min.js"></script>
-    <script src="https://unpkg.com/@swup/js-plugin/dist/SwupJsPlugin.min.js"></script>
-    <script defer src="/main.js"></script>
     <title>{title}</title>
 
     <!-- Sitemap -->
