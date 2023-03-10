98cbe042d00bb612f73cd742050f5950c72de731
Oblivious <thanos.papap42@gmail.com>
2023-03-10 21:01:52 +0200

    First concept of word fade away animation.
    TODO -> Insert a list with animation options to differentiate even more.

--- src/components/About/IntroDescription.svelte
+++ src/components/About/IntroDescription.svelte
@@ -8,7 +8,7 @@
     onMount(() => {
         gsap.registerPlugin(ScrollTrigger);
 
-        gsap.fromTo(".intro-description p", {
+        gsap.fromTo(".intro-description .intro-line", {
             yPercent: 100,
             opacity: 0,
         }, {
@@ -17,6 +17,42 @@
             opacity: 1,
             stagger: 0.05,
         });
+
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: ".intro-description",
+                start: "top top",
+                end: "+=2500",
+                // markers: true,
+                pin: true,
+                scrub: true,
+            },
+        }).fromTo(".intro-word", {
+            xPercent: 0,
+            yPercent: 0,
+            rotate: 0,
+            rotateX: 0,
+            scale: 1,
+            opacity: 1,
+        }, {
+            xPercent: -400,
+            yPercent: -800,
+            rotate: -25,
+            rotateX: 80,
+            opacity: 0,
+            scale: 1.4,
+            stagger: 0.05,
+        })
+        .add("at-bold-word-repositioning")
+        .to(".technical", {
+            xPercent: -80,
+            yPercent: 150,
+        }, "at-bold-word-repositioning")
+        .to([".artistic", ".interactive"], {
+            xPercent: -80,
+            yPercent: 150,
+            opacity: 0,
+        }, "at-bold-word-repositioning");
     });
 
     let class_prop_warning_fix = "";
@@ -25,26 +61,79 @@
 <div this={class_prop_warning_fix}></div>
 
 <div class="intro-description">
-    <p>I am a software developer and designer</p>
-    <p>blending technical and artistic skills with</p>
-    <p><span>{years_of_experience}</span> years of building interactive</p>
-    <p>experiences.</p>
+    <div class="intro-line">
+        <div class="intro-word">I</div>
+        <div class="intro-word">am</div>
+        <div class="intro-word">a</div>
+        <div class="intro-word">software</div>
+        <div class="intro-word">developer</div>
+        <div class="intro-word">and</div>
+        <div class="intro-word">designer</div>
+    </div>
+    <div class="intro-line">
+        <div class="intro-word">blending</div>
+        <div class="technical">technical</div>
+        <div class="intro-word">and</div>
+        <div class="artistic">artistic</div>
+        <div class="intro-word">skills</div>
+        <div class="intro-word">with</div>
+    </div>
+    <div class="intro-line">
+        <div class="intro-word">{years_of_experience}</div>
+        <div class="intro-word">years</div>
+        <div class="intro-word">of</div>
+        <div class="intro-word">building</div>
+        <div class="interactive">interactive</div>
+    </div>
+    <div class="intro-line">
+        <div class="intro-word">experiences.</div>
+    </div>
     <br>
-    <p>I vow to create dynamic systems that</p>
-    <p>interact in meaningful and internalized</p>
-    <p>ways with the individual.</p>
+    <div class="intro-line">
+        <div class="intro-word">I</div>
+        <div class="intro-word">vow</div>
+        <div class="intro-word">to</div>
+        <div class="intro-word">create</div>
+        <div class="intro-word">dynamic</div>
+        <div class="intro-word">systems</div>
+        <div class="intro-word">that</div>
+    </div>
+    <div class="intro-line">
+        <div class="intro-word">interact</div>
+        <div class="intro-word">in</div>
+        <div class="intro-word">meaningful</div>
+        <div class="intro-word">and</div>
+        <div class="intro-word">internalized</div>
+    </div>
+    <div class="intro-line">
+        <div class="intro-word">ways</div>
+        <div class="intro-word">with</div>
+        <div class="intro-word">the</div>
+        <div class="intro-word">individual.</div>
+    </div>
 </div>
 
 <style lang="scss">
-    div {
+    .intro-description {
         padding-top: 25vh;
-        height: 300vh;
         visibility: hidden;
-
-        span {
-            font-style: italic;
-        }
-
         transform: translate(17vw);
+    }
+
+    .intro-line {
+        display: flex;
+        gap: 0.7vw;
+    }
+
+    .intro-word {
+        font-size: 2.5vw;
+        font-weight: 500;
+    }
+
+    .technical,
+    .artistic,
+    .interactive {
+        font-size: 2.5vw;
+        font-weight: 900;
     }
 </style>
