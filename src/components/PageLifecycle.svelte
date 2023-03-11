dbccc19ffd1fae20ae37dde02b1ac5603205ee5f
Oblivious <thanos.papap42@gmail.com>
2023-03-11 16:33:48 +0200

    Moved layout animations into PageLifecycle as a client side component.

--- src/components/PageLifecycle.svelte
+++ src/components/PageLifecycle.svelte
@@ -6,10 +6,88 @@
 
     onMount(() => {
         $underline_value = current_page;
+
+        gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+        // Smooth Scrollbar + GSAP ScrollTrigger
+        const scroll_smoother = ScrollSmoother.create({
+            smooth: 1.25,
+            effects: true,
+            smoothTouch: true,
+        });
+
+        const to_about_in = next => {
+            gsap.to("#swup", {
+                onComplete: next,
+            });
+        }
+
+        const to_about_out = next => {
+            gsap.to("#swup", {
+                duration: 0.95,
+                onComplete: next,
+            });
+
+            gsap.to("#work-section", {
+                duration: 0.8,
+                opacity: 0,
+                yPercent: -300,
+                stagger: 0.015,
+                ease: "power2.inOut",
+            });
+        }
+
+        const to_work_in = next => {
+            gsap.fromTo("#swup", {
+                duration: 0.8,
+                autoAlpha: 0,
+            }, {
+                ease: "power1.out",
+                autoAlpha: 1,
+                yPercent: 0,
+                onComplete: next,
+            });
+
+            gsap.fromTo("#work-section", {
+                yPercent: 120,
+            }, {
+                ease: "power1.out",
+                yPercent: 0,
+                stagger: 0.075,
+            });
+        }
+
+        const to_work_out = next => {
+            gsap.to("#swup", {
+                duration: 0.8,
+                ease: "power2.inOut",
+                autoAlpha: 0,
+                onComplete: next,
+            });
+        }
+
+        // TODO Try fix this (scroll 0 instead of 1)
+        const start_from_page_top = () => scroll_smoother.scrollTop(1);
+
+        new Swup({
+            animateHistoryBrowsing: true,
+            plugins: [
+                new SwupHeadPlugin(),
+                new SwupJsPlugin([
+                    {
+                        from: "/",
+                        to: "/about",
+                        in: (next, _infos) => to_about_in(next),
+                        out: (next, _infos) => to_about_out(next),
+                    },
+                    {
+                        from: "/about",
+                        to: "/",
+                        in: (next, _infos) => to_work_in(next),
+                        out: (next, _infos) => to_work_out(next),
+                    },
+                ]),
+            ],
+        }).on("pageView", start_from_page_top);
     });
-
-    let class_prop_warning_fix = "";
-    export { class_prop_warning_fix as class };
 </script>
-
-<div class:class_prop_warning_fix></div>
