28e792bc36787a578c2f7e5d9c11842a8323b062
Oblivious <thanos.papap42@gmail.com>
2023-03-11 22:13:51 +0200

    Fixed class prop warning (not showing on production).

--- src/components/About/ComputerRelatedTopicsIEnjoy.svelte
+++ src/components/About/ComputerRelatedTopicsIEnjoy.svelte
@@ -13,9 +13,6 @@
             opacity: 1,
         });
     });
-
-    let class_prop_warning_fix = "";
-    export { class_prop_warning_fix as class };
 </script>
 
 <div class="computer-related-topics-i-enjoy">
