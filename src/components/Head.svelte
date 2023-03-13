f382e56fe6954b309cb38f66b9a6944b73bd9322
Oblivious <thanos.papap42@gmail.com>
2023-03-13 03:07:04 +0200

    Revamped `head` element.

--- src/components/Head.svelte
+++ src/components/Head.svelte
@@ -5,33 +5,41 @@
 <!-- Common -->
 <head>
     <meta charset="UTF-8">
-    <title>{title}</title>
-    <meta name="description" content={"Thanasis's Portfolio"}>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!-- Viewport -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Description -->
+    <meta name="description" content="Portfolio of Thanasis Papapostolou">
+
+    <!-- Page Icon -->
     <link rel="icon" type="image/svg+xml" href="/favicon.svg">
 
     <!-- GSAP -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.4/gsap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.4/ScrollTrigger.min.js"></script>
-    <script src="/ScrollSmoother.min.js"></script>
+    <script is:inline src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.4/gsap.min.js"></script>
+    <script is:inline src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.4/ScrollTrigger.min.js"></script>
+    <script is:inline src="/ScrollSmoother.min.js"></script>
 
     <!-- Swup -->
-    <script src="https://unpkg.com/swup@3"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@swup/js-plugin@1.0.5/dist/SwupJsPlugin.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@swup/head-plugin@1.3.0/dist/SwupHeadPlugin.min.js"></script>
+    <script is:inline src="https://unpkg.com/swup@3"></script>
+    <script is:inline src="https://cdn.jsdelivr.net/npm/@swup/js-plugin@1.0.5/dist/SwupJsPlugin.min.js"></script>
+    <script is:inline src="https://cdn.jsdelivr.net/npm/@swup/head-plugin@1.3.0/dist/SwupHeadPlugin.min.js"></script>
 
     <!-- Sitemap -->
-    <link rel="sitemap" href="/sitemap.xml">
+    <!-- <link rel="sitemap" href="/sitemap.xml"> -->
 
     <!-- RSS -->
-    <link rel="alternate" type="application/rss+xml" href="/feed/posts.xml">
+    <!-- <link rel="alternate" type="application/rss+xml" href="/feed/posts.xml"> -->
 
-    <!-- SEO -->
+    <!-- og and twitter SEO -->
     <meta property="og:title" content={title}>
-    <meta property="og:description" content={"Thanasis's Portfolio"}>
+    <meta property="og:description" content="Portfolio of Thanasis Papapostolou">
 
     <meta name="twitter:site" content="@astro">
     <meta name="twitter:title" content={title}>
-    <meta name="twitter:description" content={"Thanasis's Portfolio"}>
+    <meta name="twitter:description" content="Portfolio of Thanasis Papapostolou">
 
+    <!-- Title -->
     <title>{title}</title>
 </head>
