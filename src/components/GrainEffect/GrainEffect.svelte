5f5f5c3ae828063d68718dea12a8085f5edb70c7
Oblivious <thanos.papap42@gmail.com>
2023-03-15 23:06:57 +0200

    Abstracted grain effect into its own file.

---
+++ src/components/GrainEffect/GrainEffect.svelte
@@ -0,0 +1,19 @@
+<div class="grain">
+    <div class="grain-pattern"></div>
+</div>
+
+<style>
+    .grain {
+        position: fixed;
+        width: 100%;
+        height: 5000vh;
+        opacity: 0.03;
+    }
+
+    .grain-pattern {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        background-image: url("/grain.gif");
+    }
+</style>
