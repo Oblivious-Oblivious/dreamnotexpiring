e808f0b777c38c5d33e24d37fa5aaeb9ae2d2788
Oblivious <thanos.papap42@gmail.com>
2023-03-16 03:57:25 +0200

    Update Image.svelte

--- src/components/CrtIntro/Image.svelte
+++ src/components/CrtIntro/Image.svelte
@@ -1,19 +1,16 @@
 <script>
     import { onMount } from "svelte";
 
-    var crt_source;
+    var crt;
 
     onMount(() => {
-        /* TODO 1 Add image content with css instead of javascript */
-        if(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches)
-            crt_source = "crt-dark.jpg";
-        else
-            crt_source = "crt-light.jpg";
+        crt = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches
+            ? "/crt-dark.jpg"
+            : "/crt-light.jpg";
     });
 </script>
 
-<!-- TODO 1 Add image content with css instead of javascript -->
-<img class="crt" src={crt_source} alt="A 90s CRT tv">
+<img class="crt" src={crt} alt="A 90s CRT tv">
 
 <style>
     img {
