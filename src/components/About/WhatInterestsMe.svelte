0a11adba0db85ca8814aab55015e9a64e7d76f0a
Oblivious <thanos.papap42@gmail.com>
2023-03-19 15:41:38 +0200

    Abstracted bold word into its own component.

--- src/components/About/WhatInterestsMe.svelte
+++ src/components/About/WhatInterestsMe.svelte
@@ -1,17 +1,12 @@
 <script>
     import { onMount } from "svelte";
+    import InterestBoldWord from "./WhatInterestsMe/InterestBoldWord.svelte";
     import SoftwareLinks from "./WhatInterestsMe/SoftwareLinks.svelte";
     import ParallaxMixcloudLinks from "./WhatInterestsMe/ParallaxMixcloudLinks.svelte";
 
     onMount(() => {
-        gsap.registerPlugin(ScrollTrigger);
-
         gsap.set(".what-interests-me", {
             y: "-25vh",
-        });
-
-        gsap.set(".interest-bold-word", {
-            xPercent: 320,
         });
 
         gsap.fromTo(".what-interests-me", {
@@ -20,102 +15,11 @@
             opacity: 1,
             autoAlpha: 1,
         });
-
-        gsap.timeline({
-            scrollTrigger: {
-                trigger: ".interest-bold-word",
-                endTrigger: ".how-i-learn",
-                start: "top 18%",
-                end: "+=3900",
-                // markers: true,
-                pin: true,
-                scrub: true,
-            },
-        })
-        .from(".interest-technical .interest-letter", {
-            opacity: 0,
-            y: 80,
-            rotateX: -90,
-            stagger: 0.015,
-        }, "<")
-        .to(".interest-technical .interest-letter", {
-            opacity: 0,
-            delay: 0.1,
-            y: -80,
-            rotateX: 90,
-            stagger: 0.015,
-        }, "<1")
-        .from(".interest-interactive .interest-letter", {
-            opacity: 0,
-            y: 80,
-            rotateX: -90,
-            stagger: 0.015,
-        }, "<")
-        .to(".interest-interactive .interest-letter", {
-            delay: 0.2,
-            opacity: 0,
-            y: -80,
-            rotateX: 90,
-            stagger: 0.015,
-        }, "<1")
-        .from(".interest-artistic .interest-letter", {
-            opacity: 0,
-            y: 80,
-            rotateX: -90,
-            stagger: 0.015,
-        }, "<")
-        .to(".interest-artistic .interest-letter", {
-            opacity: 0,
-            y: -80,
-            rotateX: 90,
-            stagger: 0.015,
-        }, "<1");
     });
 </script>
 
 <div class="what-interests-me" style="visibility: hidden;">
-    <div class="interest-bold-word">
-        <div class="interest-technical">
-            <div class="interest-letter">&nbsp;</div>
-            <div class="interest-letter">&nbsp;</div>
-            <div class="interest-letter">t</div>
-            <div class="interest-letter">e</div>
-            <div class="interest-letter">c</div>
-            <div class="interest-letter">h</div>
-            <div class="interest-letter">n</div>
-            <div class="interest-letter">i</div>
-            <div class="interest-letter">c</div>
-            <div class="interest-letter">a</div>
-            <div class="interest-letter">l</div>
-        </div>
-        <div class="interest-interactive">
-            <div class="interest-letter">i</div>
-            <div class="interest-letter">n</div>
-            <div class="interest-letter">t</div>
-            <div class="interest-letter">e</div>
-            <div class="interest-letter">r</div>
-            <div class="interest-letter">a</div>
-            <div class="interest-letter">c</div>
-            <div class="interest-letter">t</div>
-            <div class="interest-letter">i</div>
-            <div class="interest-letter">v</div>
-            <div class="interest-letter">e</div>
-        </div>
-        <div class="interest-artistic">
-            <div class="interest-letter">&nbsp;</div>
-            <div class="interest-letter">&nbsp;</div>
-            <div class="interest-letter">&nbsp;</div>
-            <div class="interest-letter">&nbsp;</div>
-            <div class="interest-letter">a</div>
-            <div class="interest-letter">r</div>
-            <div class="interest-letter">t</div>
-            <div class="interest-letter">i</div>
-            <div class="interest-letter">s</div>
-            <div class="interest-letter">t</div>
-            <div class="interest-letter">i</div>
-            <div class="interest-letter">c</div>
-        </div>
-    </div>
+    <InterestBoldWord client:load/>
 
     <div class="spacer"></div>
 
@@ -147,22 +51,6 @@
         padding-bottom: 8rem;
     }
 
-    .interest-bold-word {
-        position: absolute;
-        z-index: 9999;
-
-        .interest-technical,
-        .interest-interactive,
-        .interest-artistic {
-            font-family: "Saol Display";
-            font-size: 2.6vw;
-            font-weight: 800;
-            font-style: italic;
-            margin: 0;
-            line-height: 0;
-            display: flex;
-            gap: 0;
-        }
     }
 
     .mixology-horizontal-cards {
---
+++ src/components/About/WhatInterestsMe/InterestBoldWord.svelte
@@ -0,0 +1,124 @@
+<script>
+    import { onMount } from "svelte";
+
+    onMount(() => {
+        gsap.registerPlugin(ScrollTrigger);
+
+        gsap.set(".interest-bold-word", {
+            xPercent: 320,
+        });
+
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: ".interest-bold-word",
+                endTrigger: ".how-i-learn",
+                start: "top 18%",
+                end: "+=3900",
+                // markers: true,
+                pin: true,
+                scrub: true,
+            },
+        })
+        .from(".interest-technical .interest-letter", {
+            opacity: 0,
+            y: 80,
+            rotateX: -90,
+            stagger: 0.015,
+        }, "<")
+        .to(".interest-technical .interest-letter", {
+            opacity: 0,
+            delay: 0.35,
+            y: -80,
+            rotateX: 90,
+            stagger: 0.015,
+        }, "<1")
+        .from(".interest-interactive .interest-letter", {
+            opacity: 0,
+            y: 80,
+            rotateX: -90,
+            stagger: 0.015,
+        }, "<")
+        .to(".interest-interactive .interest-letter", {
+            delay: 0.25,
+            opacity: 0,
+            y: -80,
+            rotateX: 90,
+            stagger: 0.015,
+        }, "<1")
+        .from(".interest-artistic .interest-letter", {
+            opacity: 0,
+            y: 80,
+            rotateX: -90,
+            stagger: 0.015,
+        }, "<")
+        .to(".interest-artistic .interest-letter", {
+            opacity: 0,
+            y: -80,
+            rotateX: 90,
+            stagger: 0.015,
+        }, "<1");
+    });
+</script>
+
+<div class="interest-bold-word">
+    <div class="interest-technical">
+        <div class="interest-letter">&nbsp;</div>
+        <div class="interest-letter">&nbsp;</div>
+        <div class="interest-letter">t</div>
+        <div class="interest-letter">e</div>
+        <div class="interest-letter">c</div>
+        <div class="interest-letter">h</div>
+        <div class="interest-letter">n</div>
+        <div class="interest-letter">i</div>
+        <div class="interest-letter">c</div>
+        <div class="interest-letter">a</div>
+        <div class="interest-letter">l</div>
+    </div>
+    <div class="interest-interactive">
+        <div class="interest-letter">i</div>
+        <div class="interest-letter">n</div>
+        <div class="interest-letter">t</div>
+        <div class="interest-letter">e</div>
+        <div class="interest-letter">r</div>
+        <div class="interest-letter">a</div>
+        <div class="interest-letter">c</div>
+        <div class="interest-letter">t</div>
+        <div class="interest-letter">i</div>
+        <div class="interest-letter">v</div>
+        <div class="interest-letter">e</div>
+    </div>
+    <div class="interest-artistic">
+        <div class="interest-letter">&nbsp;</div>
+        <div class="interest-letter">&nbsp;</div>
+        <div class="interest-letter">&nbsp;</div>
+        <div class="interest-letter">&nbsp;</div>
+        <div class="interest-letter">a</div>
+        <div class="interest-letter">r</div>
+        <div class="interest-letter">t</div>
+        <div class="interest-letter">i</div>
+        <div class="interest-letter">s</div>
+        <div class="interest-letter">t</div>
+        <div class="interest-letter">i</div>
+        <div class="interest-letter">c</div>
+    </div>
+</div>
+
+<style lang="scss">
+    .interest-bold-word {
+        position: absolute;
+        z-index: 9999;
+    
+        .interest-technical,
+        .interest-interactive,
+        .interest-artistic {
+            font-family: "Saol Display";
+            font-size: 2.6vw;
+            font-weight: 800;
+            font-style: italic;
+            margin: 0;
+            line-height: 0;
+            display: flex;
+            gap: 0;
+        }
+    }
+</style>
