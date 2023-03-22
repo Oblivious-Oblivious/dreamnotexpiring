e67740550ecd8b0d02a3f1ac75a81d89a166bf82
Oblivious <thanos.papap42@gmail.com>
2023-03-22 02:32:21 +0200

    Fixed an issue where page lifecycle would reload code that should be run once.
    Moved PageLifecycle animations into an astro js script.  Now script tag executes once as a module.  ScrollSmoother is defined once only.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,4 @@
 /// <reference types="astro/client" />
+/// <reference types="gsap/types/gsap-core" />
+/// <reference types="gsap/types/scroll-trigger" />
+/// <reference types="gsap/types/scroll-smoother" />
