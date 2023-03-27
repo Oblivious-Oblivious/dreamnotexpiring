e25ba8378870fae8222381718707aa49395d8e63
Oblivious <thanos.papap42@gmail.com>
2023-03-28 02:22:19 +0300

    Added a react adapter for svelte components and astro.
    Can now inject react components inside of svelte.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import preprocess from "svelte-preprocess";
 
 // https://astro.build/config
 import svelte from "@astrojs/svelte";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,5 +15,6 @@ export default defineConfig({
                 }),
             ],
         }),
+        react(),
     ],
 });
---
+++ src/components/About/WhatInterestsMe/ReactAdapter.svelte
@@ -0,0 +1,33 @@
+<script>
+    import React from "react";
+    import ReactDOM from "react-dom/client";
+    import { onDestroy, onMount } from "svelte";
+
+    const e = React.createElement;
+    let container;
+
+    /**
+     * Svelte compiles on the server/statically, React renders on the client.
+     * Therefore, React stuff need to happen within Svelte (window/dom) zones.
+     */
+    onMount(() => {
+        const { el, children, class: _, ...props } = $$props;
+        try {
+            ReactDOM.createRoot(container).render(e(el, props, children));
+        }
+        catch (err) {
+            console.warn(`react-adapter failed to mount.`, { err });
+        }
+    });
+
+    onDestroy(() => {
+        try {
+            container && ReactDOM.unmountComponentAtNode(container);
+        }
+        catch (err) {
+            console.warn(`react-adapter failed to unmount.`, { err });
+        }
+    });
+</script>
+
+<div bind:this={container} class={$$props.class} />
