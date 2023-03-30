f02b0b349a71e73a7a911ebca5e3b2106fca31ab
Oblivious <thanos.papap42@gmail.com>
2023-03-30 03:01:59 +0300

    Removed unused blotter materials.

--- src/components/Head/Head.svelte
+++ src/components/Head/Head.svelte
@@ -31,10 +31,7 @@
 
     <!-- Blotter -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Blotter/0.1.0/blotter.js" integrity="sha512-xPlQVbjVIwT87dKt/F1NbPQfx/LNSbKa/2nBBEGYncA4r2cd9hpFh/7+4FiQS6pWWvfz8pR19H+iCWFaHOPNQg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Blotter/0.1.0/materials/channelSplitMaterial.min.js" integrity="sha512-R32Ze9rVhuQDws+nB1mlGcsddPA8eyKmnrhCov6nqaQJgzo3QdpksX0EUtrb0fTehOrwRh8yUDeZ0enMi77pSQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Blotter/0.1.0/materials/fliesMaterial.min.js" integrity="sha512-O0ZGr/FxL9gkvxJZzOohrfkQjZw+WaIvXXXLKVCASwrB8/wRnpvq1WDhoHXQ5WkHYCj+opsR+sUqkWcmtnBb/A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Blotter/0.1.0/materials/rollingDistortMaterial.min.js" integrity="sha512-oZlRi+2/UqweL66OV5K871iUNEZm7mYhFtbiUp0Oxs6gK96nPIJUGQAMFXcx0Ax0LdGMNuqeBf90kShN6GAmrg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Blotter/0.1.0/materials/slidingDoorMaterial.min.js" integrity="sha512-YaDQp342DPZndINwYGJM0WbaslkZO/jLl3w3IVuj0E79oOmVgD2ZXSFwnUFAH43vpiwFBnfcOgwGfi1+OHYRMg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
     <!-- Sitemap -->
     <!-- <link rel="sitemap" href="/sitemap.xml"> -->
