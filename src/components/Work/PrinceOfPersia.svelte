2180e76752e7a47dedd3fb9d4b15b70d4cf40131
Oblivious <thanos.papap42@gmail.com>
2023-03-31 23:56:43 +0300

    Fixed forgotten page.

--- src/components/Work/PrinceOfPersia.svelte
+++ src/components/Work/PrinceOfPersia.svelte
@@ -1,3 +1,12 @@
+<script>
+    import { onMount } from "svelte";
+    import { underline_value } from "@persistence/underline_value";
+
+    onMount(() => {
+        $underline_value = "";
+    });
+</script>
+
 <h1>PRINCE OF PERSIA ON SCRATCH</h1>
 
 <style>
