aadeb4b4d5568dfecc4b8f175019a73d87665f9f
Oblivious <thanos.papap42@gmail.com>
2023-03-31 22:13:16 +0300

    Created separate components for each page.

---
+++ src/components/Work/AeroCSS.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { onMount } from "svelte";
+    import { underline_value } from "@persistence/underline_value";
+
+    onMount(() => {
+        $underline_value = "";
+    });
+</script>
+
+<h1>AEROCSS</h1>
+
+<style>
+    h1 {
+        padding: 5vw;
+        font-size: 5vw;
+    }
+</style>
---
+++ src/components/Work/PrinceOfPersia.svelte
@@ -0,0 +1,8 @@
+<h1>PRINCE OF PERSIA ON SCRATCH</h1>
+
+<style>
+    h1 {
+        padding: 5vw;
+        font-size: 5vw;
+    }
+</style>
