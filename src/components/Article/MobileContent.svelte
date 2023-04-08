65a1abad5da1efba0fbcb102db050d98e2ff7942
Oblivious <thanos.papap42@gmail.com>
2023-04-09 02:13:12 +0300

    Added mobile equivalents for Article components.

---
+++ src/components/Article/MobileContent.svelte
@@ -0,0 +1,42 @@
+<script>
+    export let headline;
+    export let description;
+
+    import { onMount } from "svelte";
+    import Line from "./Line.svelte";
+
+    let headline_element;
+    let description_element;
+
+    onMount(() => {
+        gsap.from([headline_element, description_element], {
+            delay: 3.1,
+            duration: 1.25,
+            opacity: 0,
+        });
+    });
+</script>
+
+<br><br>
+<div bind:this={headline_element} class="headline">
+    {headline}
+</div>
+<Line/>
+<div bind:this={description_element} class="description">
+    {description}
+    <slot/>
+</div>
+
+<style>
+    .headline {
+        display: flex;
+        font-family: var(--display-font);
+        font-size: 8vw;
+        text-align: center;
+    }
+
+    .description {
+        font-size: 5vw;
+        text-align: justify;
+    }
+</style>
---
+++ src/components/Article/MobileTitle.svelte
@@ -0,0 +1,60 @@
+<script>
+    export let name;
+    export let link;
+    export let link_type;
+    
+    import { onMount } from "svelte";
+
+    onMount(() => {
+        gsap.timeline({
+            delay: 0.3,
+        })
+        .from(".title", {
+            duration: 0.65,
+            clipPath: "inset(0 100% 0 100%)",
+        });
+    });
+</script>
+
+<div class="title">
+    <div class="name">
+        {name}
+    </div>
+</div>
+<div class="link">
+    <a href={link} target="_blank" rel="noopener noreferrer" draggable="false">
+        &lt;{link_type}&gt;
+    </a>
+</div>
+
+<style>
+    .title {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .name {
+        font-size: 16vw;
+        font-family: var(--display-font);
+        font-style: italic;
+    }
+
+    .link {
+        font-size: 7vw;
+        animation: under_line 1s infinite;
+        z-index: 9999;
+    }
+
+    @keyframes under_line {
+        0% {
+            text-decoration: none;
+        }
+        50% {
+            text-decoration: underline;
+        }
+        100% {
+            text-decoration: none;
+        }
+    }
+</style>
